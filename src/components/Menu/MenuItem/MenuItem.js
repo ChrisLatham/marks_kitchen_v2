5f61b30c18a1a8c7b73d625e363a7c06861a9262
ChrisLatham <chris@chrislatham.co.uk>
2020-08-02 23:30:29 +0100

    Parse price to GBP. Only show description div is description available.

--- src/components/Menu/MenuItem/MenuItem.js
+++ src/components/Menu/MenuItem/MenuItem.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./MenuItem.css";
 
 const menuItem = ({ id, title, description, price }) => {
+  let oDescription = null;
+  if (description !== "") {
+    oDescription = <div className="menu-item-description">{description}</div>;
+  }
+
+  const parsedPrice = parseFloat(price).toLocaleString("en-GB", {
+    style: "currency",
+    currency: "GBP",
+  });
+
   return (
     <div className="menu-item">
       <div className="menu-item-id">{id}</div>
       <div className="menu-item-title">{title}</div>
-      <div className="menu-item-description">{description}</div>
-      <div className="menu-item-price">{price}</div>
+      {oDescription}
+      <div className="menu-item-price">{parsedPrice}</div>
     </div>
   );
 };
